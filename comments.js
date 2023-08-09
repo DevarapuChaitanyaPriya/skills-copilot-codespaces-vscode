8a15ff30e7c546e0def74500cb2cd530ec7bdff4
DevarapuChaitanyaPriya <131691679+DevarapuChaitanyaPriya@users.noreply.github.com>
2023-08-09 17:30:07 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,4 +1,5 @@
 //create web server 
 const express =require('express');
 const router =express.Router();
+const commentController=require('../controllers/comment')
 router.get('/',commentController.comment_list);
