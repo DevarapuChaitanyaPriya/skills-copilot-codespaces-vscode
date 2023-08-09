62dacb1d7437d34e746488eaaa548ba229728a1d
DevarapuChaitanyaPriya <131691679+DevarapuChaitanyaPriya@users.noreply.github.com>
2023-08-09 17:35:31 +0000

    okay

--- comments.js
+++ comments.js
@@ -1,5 +1,15 @@
-//create web server 
-const express =require('express');
-const router =express.Router();
-const commentController=require('../controllers/comment')
-router.get('/',commentController.comment_list);
+//Create web server 
+const express = require('express');
+const commentController = require('./controllers/comment'); // Replace with the correct path to your comment controller module
+
+// Create an instance of Express
+const app = express();
+const port = 3000; // Change this to the desired port number
+
+// Define a route for handling GET requests to the root path
+app.get('/', commentController.comment_list);
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
